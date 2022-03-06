f1a5cfcf4c5fa633fa990dbce4c0c8c0b94980df
Vinícius <vinicius.nevesco@gmail.com>
2022-03-06 16:54:32 -0300

    Unsplash API (URL)

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ meta tags
 icons
 formsubmit
 unsplash api
+node fetch
 google fonts
 
 favicon: <div> undefined <a href="https://www.freepik.com" title="Freepik"> Freepik </a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com'</a></div>
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,31 @@
-"use strict"
+"use strict";
+
+// API UNSPLASH
+const url = 'https://api.unsplash.com/search/photos/?query=gift&per_page=6&client_id=v_a_QuzuHm-scFMvZQHkh3h9UmqEpIdylo8YBa7kDkQ';
+
+
+// DOM
+const btn = document.querySelector('.header__button');
+const cardsCover = document.querySelectorAll('.main__card__cover');
+
+btn.addEventListener('click', () => {
+  fetch(url) 
+
+  .then(response => {
+    // console.log(response);
+    if(response.ok === true) {
+      return response.json();
+    }
+    else {
+      alert(response.status);
+    }
+  })
+
+  .then(data => {
+    const imageNodes = []
+    for(let i = 0; i < data.results.length; i++){
+      cardsCover[i].style.backgroundImage = 'url('+data.results[i].urls.raw+')';
+      console.log(data.results[i].urls.raw);
+    }
+  })
+})
