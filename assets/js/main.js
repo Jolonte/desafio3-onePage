edeb1636aec0287cfb33ee23b892a00e1e7ca3ab
Vinícius <vinicius.nevesco@gmail.com>
2022-03-06 19:12:17 -0300

    Fix bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,15 @@
 "use strict";
 
-// API UNSPLASH
-const url = 'https://api.unsplash.com/photos/random/?count=6&client_id=v_a_QuzuHm-scFMvZQHkh3h9UmqEpIdylo8YBa7kDkQ';
+import api from './api.js';
 
+// API UNSPLASH
+const url = `https://api.unsplash.com/photos/random/?orientation=landscape&count=6&client_id=${api}`;
 
 // DOM
 const btn = document.querySelector('.header__button');
 const cardsLinks = document.querySelectorAll('.main__a');
 const cardsCover = document.querySelectorAll('.main__card__cover');
+const cardsH2 = document.querySelectorAll('.main__card__h2');
 const cardsP = document.querySelectorAll('.main__card__p');
 
 btn.addEventListener('click', () => {
@@ -16,8 +18,10 @@ btn.addEventListener('click', () => {
   .then((data) => {
     console.log(data);
     for(let i = 0; i < data.length; i++){
-      cardsLinks[i].href = data[i].links.html;
+      cardsLinks[i].href = data[i].links.download;
+      cardsLinks[i].target = '_blank'
       cardsCover[i].style.backgroundImage = 'url('+data[i].urls.regular+')';
+      cardsH2[i].innerText = `${data[i].user.name}`
       cardsP[i].innerHTML = `${data[i].description}<br><br>Downloads: ${data[i].downloads}<br><br>Likes: ${data[i].likes}`;
     }
   })
